68442a2d48ae2a4383f0dd661d875f10678c88ed
MStarmans91 <m.starmans@erasmusmc.nl>
2019-05-08 09:19:11 +0200

    Updated tutorial to WORC 3.0 and thus Python3.

--- WORC_example.py
+++ WORC_example.py
@@ -3,37 +3,44 @@ import fastr
 import glob
 import os
 
-# NOTE: This example is just to condense the content of the notebook to a script you can use.
+# NOTE: This example is just to condense the content of the WORCTutorial
+# Jupyter notebook to a script you can use.
 
-# NOTE: please give the below value the name of your account on Ubuntu, such that the home folder can be correctly found
-username = 'worc'
+home = os.path.expanduser('~')
 
 # Create network and use simplest plugin for execution.
 network = WORC.WORC('example')
 network.fastr_plugin = 'LinearExecution'
 
-# Below are the minimal inputs: you can also use features instead of images and segmentations.
-# You need to change these to your sources: check the FASTR documentation.
-# See the WORC Github Wiki for specifications of the various attributes, such as the ones used below
+# Below are the minimal inputs:
+#   - Images
+#   - Segmentations
+#   - Labels
+# Instead of images and segmenations you can also use features.
 
-# image_sources = glob.glob(('/home/{}/Documents/Data/STWStrategyMMD/*/image.nii.gz').format(username))
-# image_sources = [i.replace(('/home/{}').format(username), 'vfs://home') for i in image_sources]
-# image_sources = {os.path.basename(os.path.dirname(i)): i for i in image_sources}
-home = os.path.expanduser('~')
-subject_labels = ['interobs' + num for num in ['05', '06', '08', '09', '10', '11', '12', '13', '15']]
-datafolder = os.path.join(home, 'Documents', 'Data', 'STWStrategyMMD')
-image_sources = {k: r'vfs://home/Documents/Demo/worc/image.nii.gz' for k in subject_labels}
+# Below, we are going to tell WORC where your files for these inputs are locatedself.
+# Here, we assume you have a folder $HOME/Documents/Data/StrategyMMD
+# (or with \\ path separators on Windows), in which there is a folder for each
+# patient, where in each folder there is a image.nii.gz and a mask.nii.gz
+
+# You may need to change these to your files, see also the https://worc.readthedocs.io/en/latest/
+# on how to do so.
+
+# Search for the files
+image_sources = glob.glob(os.path.join(home, 'Documents', 'Data', 'STWStrategyMMD', '*', 'image.nii.gz'))
+segmentation_sources = glob.glob(os.path.join(home, 'Documents', 'Data', 'STWStrategyMMD', '*', 'mask.nii.gz'))
+
+# Create a dictionary where the keys are the patient names, so they can be matched with the labels
+image_sources = {os.path.basename(os.path.dirname(i)): i for i in image_sources}
+segmentation_sources = {os.path.basename(os.path.dirname(i)): i for i in segmentation_sources}
+
+# Append to the network
 network.images_train.append(image_sources)
-
-# segmentation_sources = glob.glob(('/home/{}/Documents/Data/STWStrategyMMD/*/mask.nii.gz').format(username))
-# segmentation_sources = [i.replace(('/home/{}').format(username), 'vfs://home') for i in segmentation_sources]
-# segmentation_sources = {os.path.basename(os.path.dirname(i)): i for i in segmentation_sources}
-segmentation_sources = {k: r'vfs://home/Documents/Demo/worc/seg_laesie_S2_v1.16_Sanne_20181121_0930.nii.gz' for k in subject_labels}
 network.segmentations_train.append(segmentation_sources)
 
-pinfo_file = os.path.join(datafolder, 'pinfo.txt')
+# We assume the labels file is in the same data folder as your patient folders.
+pinfo_file = os.path.join(home, 'Documents', 'Data', 'STWStrategyMMD', 'pinfo.txt')
 network.labels_train.append(pinfo_file)
-# network.labels_train.append('vfs://home/Documents/WORCTutorial/Data/StrategyMMD/pinfo.txt')
 
 # WORC passes a config.ini file to all nodes which contains all parameters.
 # You can get the default configuration with the defaultconfig function
@@ -44,7 +51,7 @@ config = network.defaultconfig()
 # See the Github Wiki for all available fields and their explanation.
 config['SampleProcessing']['SMOTE'] = 'False'
 config['CrossValidation']['N_iterations'] = '5'
-config['Genetics']['label_names'] = 'imaginary_label_1'
+config['Labels']['label_names'] = 'imaginary_label_1'
 config['HyperOptimization']['test_size'] = '0.3'
 network.fastr_plugin = 'LinearExecution'
 
