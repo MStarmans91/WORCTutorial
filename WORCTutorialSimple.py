ed358fb3e3903543d2ef193d467cb5403cd2ab00
MStarmans91 <m.starmans@erasmusmc.nl>
2022-04-05 12:40:28 +0200

    Update some messaging

--- WORCTutorialSimple.py
+++ WORCTutorialSimple.py
@@ -166,7 +166,7 @@ def main():
     # Read the overall peformance
     performance_file = os.path.join(experiment_folder, 'performance_all_0.json')
     if not os.path.exists(performance_file):
-        raise ValueError('No performance file found: your network has failed.')
+        raise ValueError(f'No performance file {performance_file} found: your network has failed.')
 
     with open(performance_file, 'r') as fp:
         performance = json.load(fp)
